26c1fb8b67e9804fcb96e2da30dad86f499b5af2
zacky77-git <miyazaki.ytmym@me.com>
2020-12-15 12:13:16 +0900

    userモデルの単体テスト記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates :nickname, presence: true, length: {maximum: 40 }
   validates :first_name_kanzi, presence: true
   validates :family_name_kanzi, presence: true
   validates :first_name_kana, presence: true
