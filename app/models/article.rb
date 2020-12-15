82716a6dc68d60f6cfadfc117d7660f34f016908
zacky77-git <miyazaki.ytmym@me.com>
2020-12-15 18:50:51 +0900

    バリデーションを設定

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+# class Article < ApplicationRecord
+  # extend ActiveHash::Associations::ActiveRecordExtensions
+  # belongs_to :genre
+
+  # validates :title, :text, presence: true
+  # validates :genre_id, numericality: { other_than: 1 }
+# end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
 
   belongs_to :user
   has_one_attached :image
 
+  validates :genre_id, numericality: { other_than: 1 }
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :category_id
+    validates :text
+    validates :state_id
+    validates :delivery_charge_id
+    validates :area_id
+    validates :send_date_id
+    validates :image
+  end
 end
