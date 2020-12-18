a9a528ca5d7eb2054ef2adf7fdaee3c9edb09c2f
zacky77-git <miyazaki.ytmym@me.com>
2020-12-18 12:16:08 +0900

    出品者以外商品を削除できないように記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: :edit
+  before_action :move_to_index, only: [:edit, :destroy]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
   def index
     @items = Item.includes(:user).order("created_at DESC")
